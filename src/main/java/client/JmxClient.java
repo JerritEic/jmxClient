9ea8f595e4abab3f7ab8a75d434f4ca4a415c074
Jesse Donkervliet <j.j.r.donkervliet@vu.nl>
2022-08-11 14:00:24 +0200

    chore: update help documentation

--- src/main/java/client/JmxClient.java
+++ src/main/java/client/JmxClient.java
@@ -24,8 +24,8 @@ public class JmxClient {
     public static void main(String[] args) throws Exception {
         Options options = new Options();
         options.addOption("help", false, "print this message");
-        options.addOption("ip", true, "Minecraft node IP address");
-        options.addOption("port", true, "Minecraft node port number");
+        options.addOption("ip", true, "Minecraft server IP address");
+        options.addOption("port", true, "Minecraft server port number");
         options.addOption("id", true, "ID of the JMX resource to access");
         options.addOption("out", true, "output directory");
         options.addOption("dur", true, "sample duration");
@@ -35,7 +35,7 @@ public class JmxClient {
 
         if (cmd.hasOption("help")) {
             HelpFormatter formatter = new HelpFormatter();
-            formatter.printHelp("ant", options);
+            formatter.printHelp("jmxClient", options);
             System.exit(0);
         }
 
