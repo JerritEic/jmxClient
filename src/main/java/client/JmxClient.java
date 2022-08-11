b9ffd6730f101934579f4f91efe1875b6b204809
Jesse Donkervliet <j.j.r.donkervliet@vu.nl>
2022-08-11 19:00:53 +0200

    chore: remove unused import

--- src/main/java/client/JmxClient.java
+++ src/main/java/client/JmxClient.java
@@ -3,7 +3,6 @@ package client;
 import java.io.FileWriter;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.text.MessageFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
