588304c6b3be440ab8afdfe4b1c09afb74eb9873
Jesse Donkervliet <j.j.r.donkervliet@vu.nl>
2022-08-11 18:59:40 +0200

    fix: update run scripts to work with new cli args

--- runClientTest.sh
+++ runClientTest.sh
@@ -1,2 +1,2 @@
 java -Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.port=25585 -Dcom.sun.management.jmxremote.ssl=false -jar ./target/jmxDummyServer.jar actualTicks.csv 40000 &
-java -jar ./target/jmxClient.jar com.jerrit.Meterstick:type=DummyServer 25585 . 10000
+java -jar ./target/jmxClient.jar -id com.jerrit.Meterstick:type=DummyServer -port 25585 -out . -dur 10000
